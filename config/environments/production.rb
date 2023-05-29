0ad1c4172e0921de4ebdfe7ba426d0607ed716cc
Pedro Souza <phmartinsdesouza@gmail.com>
2023-05-29 15:44:28 -0300

    a meaningful message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
 
+  config.action_mailer.default_url_options = { host: 'heroku', port: 3000 }
+
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  devise_for :users
+  root to: "cars#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end

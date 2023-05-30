cb69c8c8ab83fa10c54d612d70203d16e1111674
Pedro Souza <phmartinsdesouza@gmail.com>
2023-05-30 12:53:24 -0300

    add new routes to display your cars and rentals

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "user/:user_id/my_cars", to: "users#car_index", as: :car_index_user
+  get "user/:user_id/my_rentals", to: "users#rental_index", as: :rental_index_user
   root to: "cars#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,5 +11,4 @@ Rails.application.routes.draw do
     resources :rentals, only: %i[new create]
     resources :reviews, only: %i[new create]
   end
-
 end

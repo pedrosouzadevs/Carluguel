6914a678066741f6829af7b2bccb136de3c1fb58
Pedro Souza <phmartinsdesouza@gmail.com>
2023-06-01 11:27:26 -0300

    testing branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_152620) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_140341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_152620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "total_price"
+    t.boolean "confirmation", default: false
     t.index ["car_id"], name: "index_rentals_on_car_id"
     t.index ["user_id"], name: "index_rentals_on_user_id"
   end
